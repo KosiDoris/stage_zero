8d47eb7d17392356a47add4f88b0b385d7972be8
KosiDoris <114844125+KosiDoris@users.noreply.github.com>
2022-10-09 19:14:34 +0100

    Create stagezer0.sh

---
+++ stagezer0.sh
@@ -0,0 +1,35 @@
+ Givename="Kosisochukwu"
+ Lastname="Ogbonna"
+ echo $Lastname $Givename
+ mkdir kosi
+ mkdir biocomputing && cd biocomputing
+ wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna
+ wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk
+ wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk 
+ mv wildtype.fna ../kosi/
+ rm -f wildtype.gbk.1
+ grep 'tatatata' wildtype.fna
+ grep 'tatatata' wildtype.fna > mutant
+ clear && history
+ ls && ls ../biocomputing
+ exit
+ figlet OGBONNA KOSI
+ mkdir compare
+ wget  https://www.bioinformatics.babraham.ac.uk/training/Introduction%20to%20Unix/unix_intro_data.tar.gz 
+ gunzip unix_intro_data.tar.gz
+ tar xvf unix_intro_data.tar
+ cd compare
+ ls  compare
+ cd seqmonk_genomes
+ ls seqmonk_genomes
+ cd ‘Saccharomyces cerevisiae’
+ ls Saccharomyces cerevisiae
+ cd EF4
+ ls EF4
+  grep -i 'rRNA' Mito.dat
+  cp Mito.dat ../../../
+ nano Mito.dat
+Ctrl-X, Y (Yes), enter
+mv Mito.dat Mitochondrion.txt
+wc -l lane8_DD_P4_TTAGGC_L008_R1.fastq.gz
+wc -l lane8_DD_P4_TTAGGC_L008_R1.fastq.gz lane8_DD_P9_TGACCA_L008_R1.fastq.gz  lane8_FF_P4_ATCACG_L008_R1.fastq.gz lane8_FF_P9_CGATGT_L008_R1.fastq.gz lane8_DD_P4_TTAGGC_L008_R2.fastq.gz lane8_DD_P9_TGACCA_L008_R2.fastq.gz lane8_FF_P4_ATCACG_L008_R2.fastq.gz lane8_FF_P9_CGATGT_L008_R2.fastq.gz > newfile
